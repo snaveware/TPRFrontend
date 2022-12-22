6d95d88c05e89e800e3d2865ab21d1750dbe2415
Evans <work.evans020@gmail.com>
2022-12-23 00:32:51 +0300

    changed backed base url

--- API Calls/Config.js
+++ API Calls/Config.js
@@ -2,7 +2,9 @@
  * configure Base URL in local Storage
  */
 
-var BASE_URL = "https://tprbackend.snaveware.com";
+// var BASE_URL = "https://tprbackend.snaveware.com";
+var BASE_URL = "https://tprbackend-production.up.railway.app";
+
 // var BASE_URL = "http://localhost:5000";
 
 localStorage.setItem("BASE_URL", BASE_URL);
