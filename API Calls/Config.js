4cecc6c311ba565c4526e048026695ff3cc77322
Evans <work.evans020@gmail.com>
2022-12-23 09:29:20 +0300

    changed backend url, reviewed placeholders for register and login inputs

--- API Calls/Config.js
+++ API Calls/Config.js
@@ -2,8 +2,7 @@
  * configure Base URL in local Storage
  */
 
-// var BASE_URL = "https://tprbackend.snaveware.com";
-var BASE_URL = "https://tprbackend-production.up.railway.app";
+var BASE_URL = "https://tprbackend.snaveware.com";
 
 // var BASE_URL = "http://localhost:5000";
 
